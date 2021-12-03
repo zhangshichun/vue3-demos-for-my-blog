9e106250d65ad823cf1c0ead5f70920e99e21c45
zhangshichun <zhangshichun_looper@aliyun.com>
2021-12-03 08:32:04 +0800

    feat: add comment

--- src/demos/pixi-tool/index.vue
+++ src/demos/pixi-tool/index.vue
@@ -1,4 +1,5 @@
 <template>
+<!-- 请一定看下面的注释，重要重要重要！ -->
   <div>
     <test :path="xiuxianImg" :colorCount="6" :unitPx="4"></test>
   </div>
@@ -10,6 +11,12 @@
   </div>
 </template>
 <script setup>
+/** 
+ * 你好，年轻的勇士。
+ * 如果你正在尝试跑这段代码，我必须提醒你:
+ * 1. 因为我太菜，所以目前我只测试了 256*256 像素的图片
+ * 2. unitPx 目前只支持 2、4、8、16... 这种2的倍数，一旦不是倍数就会发生bug.....
+ */
 import Test from './test.vue'
 import xiuxianImg from './imgs/xiuxian.png'
 import catImg from './imgs/cat.png'
