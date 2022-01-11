7e6c8fa6befc781f32a9985b46110cb2727ae331
zhangshichun <zhangshichun_looper@aliyun.com>
2022-01-11 22:12:02 +0800

    feat: 实现了列内容点击后统一抛出点击事件的效果

--- src/demos/el-table-wrap/functional/index.js
+++ src/demos/el-table-wrap/functional/index.js
@@ -1,9 +1,52 @@
-// import { h } from 'vue'
+// import { h } from "vue"
+import { get } from 'lodash'
+// import { ElButton } from 'element-plus'
+
+// 1.动态实现法
+const genClickableVNode = (vNode, onClick) => {
+  vNode.children = {
+    default: ({ row }) => {
+      return <el-button type='text' onClick={() => onClick({ prop: vNode?.props.prop, row }) }>
+        { get(row, vNode?.props.prop) }
+      </el-button>    }
+  }
+  /**
+   * ShapeFlags.SLOTS_CHILDREN = 32
+   * 下面这是 vue3.x 里关于 vNode 类型的判断语法
+   */
+  if ((vNode.shapeFlag & 32) == 0) { // 当前如果不是插槽类vNode
+    vNode.shapeFlag = vNode.shapeFlag | 32 // 让它成为插槽类vNode
+  }
+  return vNode
+}
+
+// 2.生成新的VNode,继承老vNode的各种属性，且插入需要的插槽和事件
+// const genClickableVNode = (vNode, onClick) => {
+//   const slots = {
+//     default: ({ row }) => {
+//       return <el-button type='text' onClick={() => onClick({ prop: vNode?.props.prop, row }) }>
+//         { get(row, vNode?.props.prop) }
+//       </el-button>
+//     }
+//   }
+//   return <el-table-column {...vNode.attrs} {...vNode.props}>
+//     { slots }
+//   </el-table-column>
+// }
+
+
 const FunctionalTable = (props, context) => {
   const vNodes = context.slots.default()
   const filteredVNodes = props.visibleKeys == null ? vNodes : vNodes.filter(node => props.visibleKeys.includes(node?.props?.prop))
+  const onColumnClick = (...args) => {
+    context.emit('column-click', ...args)
+  }
+  const clickableVNodes = filteredVNodes.map(node => {
+    return genClickableVNode(node, onColumnClick)
+  })
+
   return <el-table {...context.attrs}>
-    { filteredVNodes }
+    { clickableVNodes }
   </el-table>
 }
 
@@ -13,5 +56,7 @@ FunctionalTable.props = {
     default: () => null
   } 
 }
+
+FunctionalTable.emit = ['column-click']
 
 export default FunctionalTable;
--- src/demos/el-table-wrap/index.vue
+++ src/demos/el-table-wrap/index.vue
@@ -1,10 +1,26 @@
 <template>
+<div>
+  <h3>动态列</h3>
   <el-button @click="onClick">给我变！</el-button>
   <FunctionalTable :data="tableData" :visibleKeys="visibleKeys" :style="{width: `100%`}">
-    <el-table-column prop="date" label="Date" width="180" />,
-    <el-table-column prop="name" label="Name" />,
+    <el-table-column prop="date" label="Date" width="180" />
+    <el-table-column prop="name" label="Name" />
     <el-table-column prop="address" label="Address" />       
-  </FunctionalTable>
+  </FunctionalTable>  
+</div>
+<div>
+  <h3>统一emit事件 (应掘金网友（id=4301761076793239）要求实现个demo)</h3>
+  <FunctionalTable :data="tableData" :style="{width: `100%`}" @column-click="onColumnClick">
+    <el-table-column prop="date" label="Date" width="180">
+      <template #default="scope">
+        <div>{{ scope.row.date }}</div>
+      </template>
+    </el-table-column>
+    <el-table-column prop="name" label="Name" />
+    <el-table-column prop="address" label="Address" />       
+  </FunctionalTable>  
+</div>
+
 </template>
 <script setup>
 import FunctionalTable from './functional';
@@ -39,4 +55,9 @@ const tableData = [
     address: 'No. 189, Grove St, Los Angeles',
   },
 ]
+
+const onColumnClick = (v) => {
+console.log("🔨🔪@zsc:: ~ file: index.vue ~ line 60 ~ onColumnClick ~ v", v)
+  
+}
 </script>
